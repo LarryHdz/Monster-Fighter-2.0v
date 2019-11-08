2e2129b8949f11f1307b4219452ea4470536d43f
Joshua-Valona <57538277+Joshua-Valona@users.noreply.github.com>
2019-11-08 13:21:50 -0600

    test
    testing

--- src/monster/Main.java
+++ src/monster/Main.java
@@ -1,21 +1,27 @@
 package monster;
 
 
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 
-		
-=======
 		System.out.println("Monster FIGHTER v2.0!!!!!!!");
 		
 		//testing the Dice class;
 		Dice x = new Dice();
 		System.out.println(x.roll());
->>>>>>> cd92b6d9b1a331bce8f7c5e224cc0cfa8298a2fc
+
+<<<<<<< HEAD
+			String s = "Hello";
+			
+			s = Hashing.encryptThisString(s);
+		
+			System.out.println(s);
+=======
+>>>>>>> 8331f9a9fd5d7855fabf0140aac49c9cc6fb59b3
 		
 	}
 
