798e8e5341a56e8799915fda4b58b1402bbf98be
jgonz131 <57538216+jgonz131@users.noreply.github.com>
2019-11-08 12:53:22 -0600

    worked on Stats

--- src/monster/Main.java
+++ src/monster/Main.java
@@ -3,11 +3,11 @@ package monster;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Monster FIGHTER v2.0!!!!!!!");
-		Dice x = new Dice();
-		System.out.println(x.roll());
 		
+		
+		
+	
+
 	}
 
 }
