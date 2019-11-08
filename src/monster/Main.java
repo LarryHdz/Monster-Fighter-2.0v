cd92b6d9b1a331bce8f7c5e224cc0cfa8298a2fc
Lawrence <master3715@live.com>
2019-11-08 12:56:38 -0600

    fixed main

--- src/monster/Main.java
+++ src/monster/Main.java
@@ -3,11 +3,13 @@ package monster;
 public class Main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Monster FIGHTER v2.0!!!!!!!");
 		
+		//testing the Dice class;
+		Dice x = new Dice();
+		System.out.println(x.roll());
 		
-		
-	
-
 	}
 
 }
