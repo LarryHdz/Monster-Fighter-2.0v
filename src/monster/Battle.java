869662cd84f6cc3fb3df8dfc6f9a8e602fa11f7b
Joshua-Valona <57538277+Joshua-Valona@users.noreply.github.com>
2019-11-18 12:31:27 -0600

    Updated battle class

--- src/monster/Battle.java
+++ src/monster/Battle.java
@@ -1,5 +1,18 @@
 package monster;
 
-public class Battle {
-
+public class Battle 
+{
+	PlayerMonster p1;
+	PlayerMonster p2;
+	
+	public Battle(PlayerMonster pl1, PlayerMonster pl2)
+	{
+		p1 =pl1;
+		p2 =pl2;
+	}
+	
+	
+	
+	
+	
 }
